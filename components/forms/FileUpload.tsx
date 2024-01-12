7282fc890c45aa8123498e1e1cb5e843890bd603
Vustron <runa.michaeljoshua@dnsc.edu.ph>
2024-01-12 16:32:28 +0800

    Finished create server ui

--- components/forms/FileUpload.tsx
+++ components/forms/FileUpload.tsx
@@ -19,6 +19,14 @@ const FileUpload = ({ endpoint, value, onChange }: FileUploadProps) => {
 		return (
 			<div className='relative h-20 w-20'>
 				<Image fill src={value} alt='upload' className='rounded-full' />
+
+				<button
+					onClick={() => onChange('')}
+					className='bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm'
+					type='button'
+				>
+					<X className='h-4 w-4' />
+				</button>
 			</div>
 		);
 	}
