383a19b18fc75d29e12ad3dee828e0cf1657bc4a
Vustron <runa.michaeljoshua@dnsc.edu.ph>
2024-01-13 15:07:38 +0800

    Finished edit server function

---
+++ app/api/servers/[serverId]/route.ts
@@ -0,0 +1,37 @@
+import { fetchCurrentProfile } from '@/lib/actions/FetchCurrentProfile';
+import { db } from '@/lib/actions/InitializeDB';
+import { NextResponse } from 'next/server';
+
+export async function PATCH(
+	req: Request,
+	{ params }: { params: { serverId: string } }
+) {
+	try {
+		// fetch user profile from database
+		const profile = await fetchCurrentProfile();
+
+		// init data
+		const { name, imageUrl } = await req.json();
+
+		// if there's no profile throw an error
+		if (!profile) {
+			return new NextResponse('Unauthorized', { status: 401 });
+		}
+
+		const server = await db.server.update({
+			where: {
+				id: params.serverId,
+				profileId: profile.id,
+			},
+			data: {
+				name,
+				imageUrl,
+			},
+		});
+
+		return NextResponse.json(server);
+	} catch (error) {
+		console.log('[SERVER_ID_PATCH]', error);
+		return new NextResponse('Internal Error', { status: 500 });
+	}
+}
