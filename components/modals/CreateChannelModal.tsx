24b573ab47b456aaa1a445a289fc5a9569208c26
Vustron <runa.michaeljoshua@dnsc.edu.ph>
2024-01-14 12:27:44 +0800

    Finished ui of create channel

--- components/modals/CreateChannelModal.tsx
+++ components/modals/CreateChannelModal.tsx
@@ -2,14 +2,15 @@
 
 import * as z from 'zod';
 import axios from 'axios';
+import qs from 'query-string';
 import { useForm } from 'react-hook-form';
-import { formSchema } from '@/lib/validation';
+import { formChannelSchema } from '@/lib/validation';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useParams, useRouter } from 'next/navigation';
 
 import {
 	Dialog,
 	DialogContent,
-	DialogDescription,
 	DialogFooter,
 	DialogHeader,
 	DialogTitle,
@@ -24,26 +25,36 @@ import {
 	FormMessage,
 } from '@/components/ui/form';
 
-import { useRouter } from 'next/navigation';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { useModal } from '@/hooks/useModalStore';
-import FileUpload from '@/components/forms/FileUpload';
+
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from '@/components/ui/select';
+import { ChannelType } from '@prisma/client';
 
 export const CreateChannelModal = () => {
 	// init use Modal
 	const { isOpen, onClose, type } = useModal();
 	const isModalOpen = isOpen && type === 'createChannel';
 
+	// init params
+	const params = useParams();
+
 	// init router
 	const router = useRouter();
 
 	// init form
-	const form = useForm<z.infer<typeof formSchema>>({
-		resolver: zodResolver(formSchema),
+	const form = useForm<z.infer<typeof formChannelSchema>>({
+		resolver: zodResolver(formChannelSchema),
 		defaultValues: {
 			name: '',
-			imageUrl: '',
+			type: ChannelType.TEXT,
 		},
 	});
 
@@ -51,9 +62,16 @@ export const CreateChannelModal = () => {
 	const isLoading = form.formState.isSubmitting;
 
 	// submit function
-	const onSubmit = async (values: z.infer<typeof formSchema>) => {
+	const onSubmit = async (values: z.infer<typeof formChannelSchema>) => {
 		try {
-			await axios.post('/api/servers', values);
+			const url = qs.stringifyUrl({
+				url: '/api/channels',
+				query: {
+					serverId: params?.serverId,
+				},
+			});
+
+			await axios.post(url, values);
 			form.reset();
 			router.refresh();
 			onClose();
@@ -72,56 +90,25 @@ export const CreateChannelModal = () => {
 			<DialogContent className='bg-white text-black p-0 overflow-hidden'>
 				<DialogHeader className='pt-8 px-6'>
 					<DialogTitle className='text-2xl text-center font-bold'>
-						Customize your server
+						Create Channel
 					</DialogTitle>
-
-					<DialogDescription className='text-center text-zinc-500'>
-						Give your server details with a name and an image. You can always
-						change it later.
-					</DialogDescription>
 				</DialogHeader>
-
 				<Form {...form}>
 					<form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
 						<div className='space-y-8 px-6'>
-							<div className='flex items-center justify-center text-center'>
-								<FormField
-									control={form.control}
-									name='imageUrl'
-									render={({ field }) => (
-										<FormItem>
-											<FormControl>
-												<FileUpload
-													endpoint='serverImage'
-													value={field.value}
-													onChange={field.onChange}
-												/>
-											</FormControl>
-										</FormItem>
-									)}
-								/>
-							</div>
-
 							<FormField
 								control={form.control}
 								name='name'
 								render={({ field }) => (
 									<FormItem>
 										<FormLabel className='uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70'>
-											Server Name
+											Channel name
 										</FormLabel>
-
 										<FormControl>
 											<Input
 												disabled={isLoading}
-												className='
-												bg-zinc-300/50 
-												border-0 
-												focus-visible:ring-0 
-												text-black 
-												focus-visible:ring-offset-0
-												'
-												placeholder='Enter server name'
+												className='bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0'
+												placeholder='Enter channel name'
 												{...field}
 											/>
 										</FormControl>
@@ -129,8 +116,39 @@ export const CreateChannelModal = () => {
 									</FormItem>
 								)}
 							/>
+							<FormField
+								control={form.control}
+								name='type'
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Channel Type</FormLabel>
+										<Select
+											disabled={isLoading}
+											onValueChange={field.onChange}
+											defaultValue={field.value}
+										>
+											<FormControl>
+												<SelectTrigger className='bg-zinc-300/50 border-0 focus:ring-0 text-black ring-offset-0 focus:ring-offset-0 capitalize outline-none'>
+													<SelectValue placeholder='Select a channel type' />
+												</SelectTrigger>
+											</FormControl>
+											<SelectContent>
+												{Object.values(ChannelType).map((type) => (
+													<SelectItem
+														key={type}
+														value={type}
+														className='capitalize'
+													>
+														{type.toLowerCase()}
+													</SelectItem>
+												))}
+											</SelectContent>
+										</Select>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
 						</div>
-
 						<DialogFooter className='bg-gray-100 px-6 py-4'>
 							<Button variant='primary' disabled={isLoading}>
 								Create
--- lib/validation.ts
+++ lib/validation.ts
@@ -1,3 +1,4 @@
+import { ChannelType } from '@prisma/client';
 import * as z from 'zod';
 
 export const formSchema = z.object({
@@ -7,4 +8,19 @@ export const formSchema = z.object({
 	imageUrl: z.string().min(1, {
 		message: 'Server image is required',
 	}),
+});
+
+export const formChannelSchema = z.object({
+	name: z
+		.string()
+		.min(1, {
+			message: 'Channel name is required',
+		})
+		.refine((name) => name !== 'General', {
+			message: "Channel name cannot be 'General'",
+		})
+		.refine((name) => name !== 'general', {
+			message: "Channel name cannot be 'general'",
+		}),
+	type: z.nativeEnum(ChannelType),
 });
