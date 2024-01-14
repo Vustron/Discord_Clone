f0f2f6a6d80499488784fb77e5258f989de16b25
Vustron <runa.michaeljoshua@dnsc.edu.ph>
2024-01-14 11:09:21 +0800

    Added REST API for changing roles of channel members

---
+++ app/api/members/[memberId]/route.ts
@@ -0,0 +1,68 @@
+import { fetchCurrentProfile } from '@/lib/actions/FetchCurrentProfile';
+import { NextResponse } from 'next/server';
+import { db } from '@/lib/actions/InitializeDB';
+
+export async function PATCH(
+	req: Request,
+	{ params }: { params: { memberId: string } }
+) {
+	try {
+		// fetch profile
+		const profile = await fetchCurrentProfile();
+		// init search params
+		const { searchParams } = new URL(req.url);
+		// init roles
+		const { role } = await req.json();
+		// fetch serverId
+		const serverId = searchParams.get('serverId');
+
+		if (!profile) {
+			return new NextResponse('Unauthorized', { status: 401 });
+		}
+
+		if (!serverId) {
+			return new NextResponse('Server ID Missing', { status: 401 });
+		}
+
+		if (!params.memberId) {
+			return new NextResponse('Member ID Missing', { status: 401 });
+		}
+
+		const server = await db.server.update({
+			where: {
+				id: serverId,
+				profileId: profile.id,
+			},
+			data: {
+				members: {
+					update: {
+						where: {
+							id: params.memberId,
+							profileId: {
+								not: profile.id,
+							},
+						},
+						data: {
+							role,
+						},
+					},
+				},
+			},
+			include: {
+				members: {
+					include: {
+						profile: true,
+					},
+					orderBy: {
+						role: 'asc',
+					},
+				},
+			},
+		});
+
+		return NextResponse.json(server);
+	} catch (error) {
+		console.log('[MEMBERS_ID_PATCH]', error);
+		return new NextResponse('Internal Error', { status: 500 });
+	}
+}
--- components/modals/MembersModal.tsx
+++ components/modals/MembersModal.tsx
@@ -64,7 +64,6 @@ export const MembersModal = () => {
 				url: `/api/members/${memberId}`,
 				query: {
 					serverId: server?.id,
-					memberId,
 				},
 			});
 
