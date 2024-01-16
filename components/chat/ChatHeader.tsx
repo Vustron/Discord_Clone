422efc8fe09024502e08813b401753b7d6c5e5d8
Vustron <runa.michaeljoshua@dnsc.edu.ph>
2024-01-16 11:01:34 +0800

    Finished adding the chat header

--- components/chat/ChatHeader.tsx
+++ components/chat/ChatHeader.tsx
@@ -1,4 +1,5 @@
 import { Hash } from 'lucide-react';
+import UserAvatar from '@/components/shared/UserAvatar';
 import MobileToggle from '@/components/ui/mobile-toggle';
 
 interface ChatHeaderProps {
@@ -17,6 +18,10 @@ const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
 			<MobileToggle serverId={serverId} />
 			{type === 'channel' && (
 				<Hash className='w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2' />
+			)}
+
+			{type === 'conversation' && (
+				<UserAvatar src={imageUrl} className='h-8 w-8 md:h-8 md:w-8 mr-2' />
 			)}
 			<p className='font-semibold text-md text-black dark:text-white'>{name}</p>
 		</div>
